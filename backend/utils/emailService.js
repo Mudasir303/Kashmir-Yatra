7b4011b480d1cb2f0a38670438c5292c4c5ff37b
Mudasir Ahmad <mirmudasir2120@gmail.com>
2026-03-04 21:07:47 +0530

    emailupo

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -1,33 +1,29 @@
 const nodemailer = require("nodemailer");
-const dns = require("dns");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  },
-  lookup: (hostname, options, callback) => {
-    dns.lookup(hostname, { family: 4 }, callback); // force IPv4
-  }
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
 });
 
 const sendEmail = async (options) => {
-  try {
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: options.to,
-      subject: options.subject,
-      html: options.html,
-      replyTo: options.replyTo
-    });
+    try {
+        await transporter.sendMail({
+            from: process.env.EMAIL_USER,
+            to: options.to,
+            subject: options.subject,
+            html: options.html,
+            replyTo: options.replyTo
+        });
 
-    console.log("Email sent successfully");
-  } catch (error) {
-    console.error("Email Sending Error:", error);
-  }
+        console.log("Email sent successfully");
+    } catch (error) {
+        console.error("Email Sending Error:", error);
+    }
 };
 
 module.exports = { sendEmail };
